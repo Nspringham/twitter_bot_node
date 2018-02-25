0531330784e02003b028ce6f4fbb93ffa718d05d
Nick Springham <Nspringham@aol.co.uk>
2018-02-25 18:23:09 +0000

    added User input for cmd line

--- get_bot.js
+++ get_bot.js
@@ -1,14 +1,3 @@
-
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-//
-// rl.question('What subject tweet are you interested in? ', (answer) => {
-//   console.log(`You are looking for tweets relating to: ${answer}`);
-//   rl.close();
-// });
 var Twit = require('twit');
 var config = require('./config'); // only config is needed in the two bot modules.
 var T = new Twit(config);
@@ -16,8 +5,8 @@ var T = new Twit(config);
 // ***GET METHOD***
 // param entry point for get function
 
-function getIt() {
-  var params = {q: 'Coding', count: 2};
+function getIt(user, amount) {
+  var params = {q: user, count: amount};
   //the get request to search for tweets using the params specified above
   T.get('search/tweets', params, gotData);
 
@@ -29,5 +18,19 @@ function getIt() {
     };
   };
 }
+// User input for
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What subject tweet are you interested in? ', (user) => {
+  rl.question('How many tweets do you want returned? ', (amount) => {
+    getIt(user, amount);
+    rl.close();
+  });
+});
+
 //after exports the name of the function is used
 module.exports.getIt = getIt;
