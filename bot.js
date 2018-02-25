431f8f6a85dbb77275b4f09e81d978edbd2386dd
Nick Springham <Nspringham@aol.co.uk>
2018-02-25 17:07:23 +0000

    Added tweet post function

--- bot.js
+++ bot.js
@@ -1,23 +1,56 @@
 console.log('The bot is starting\n');
 
 var Twit = require('twit');
-
 var config = require('./config');
 var T = new Twit(config);
 
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// rl.question('What subject tweet are you interested in? ', (answer) => {
+//   console.log(`You are looking for tweets relating to: ${answer}`);
+//   rl.close();
+// });
 
-var params = {
-  q: 'pancakes',
-  count: 5
- };
+// ***GET METHOD***
+// param entry point for get function
 
-//the get request to search for tweets using the params specified above
-T.get('search/tweets', params, gotData);
+// getIt();
+function getIt() {
+  var params = {q: 'Coding', count: 5};
+  //the get request to search for tweets using the params specified above
+  T.get('search/tweets', params, gotData);
 
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; ++i){
-      console.log(tweets[i].text);
-      console.log("\n");
+  function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; ++i){
+        console.log(tweets[i].text);
+        console.log("\n");
+    };
   };
-};
+}
+
+// ***POST METHOD***
+
+// tweetIt();
+// setInterval(tweetIt, 1000*20)
+function tweetIt() {
+  //floor function so it doesnt have a decimal
+  var r = Math.floor(Math.random()*100);
+  var tweet = {
+    status: 'here is a random number ' + r + ', another tweet from node.js #testingbots'
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+        console.log("something went wrong");
+    } else {
+        console.log("it worked")
+    };
+  };
+}
--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  // Enter your own accounts keys and tokens
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'UdwYi9vrkroTa3jc8XWmBISpB',
+  consumer_secret:      'lORY6GRNi7LzAtxMndN32bK6zPdXqUQu4JhqHnceSRfcrhdt8n',
+  access_token:         '967721061231071232-zMFFNB6NtYWmBpXGI1sRWelSUo9nhJA',
+  access_token_secret:  'SFifAPXR2x40RMHIeOHG3IJk5YtoMuYSdQOpceTv8AFao',
 }
