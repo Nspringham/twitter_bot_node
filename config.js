d910e36e542276467ee0c44a74af6643af501ac6
Nick Springham <Nspringham@aol.co.uk>
2018-02-25 12:12:30 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Enter your own accounts keys and tokens
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+}
