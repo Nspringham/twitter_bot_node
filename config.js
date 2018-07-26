880ae6b21a02f439160bd057526af0fe79c26924
Nick Springham <nickspringham7@gmail.com>
2018-07-26 15:35:30 +0100

    Altered keys in config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   consumer_key:         'UdwYi9vrkroTa3jc8XWmBISpB',
-  consumer_secret:      'lORY6GRNi7LzAtxMndN32bK6zPdXqUQu4JhqHnceSRfcrhdt8n',
+  consumer_secret:      '########################################',
   access_token:         '967721061231071232-zMFFNB6NtYWmBpXGI1sRWelSUo9nhJA',
-  access_token_secret:  'SFifAPXR2x40RMHIeOHG3IJk5YtoMuYSdQOpceTv8AFao',
+  access_token_secret:  '########################################',
 }
